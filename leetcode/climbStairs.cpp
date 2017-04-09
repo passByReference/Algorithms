37f3364dd4e2a4bf8989a7044bc1a5d10efef506
Technipire <passByReference@users.noreply.github.com>
2017-04-09 15:29:59 -0400

    climbStairs.cpp finished

---
+++ leetcode/climbStairs.cpp
@@ -0,0 +1,38 @@
+#include "all.h"
+
+using namespace std;
+
+unordered_map<int, int> m;
+
+int climbStairs_recursion(int n) {
+    if (n <= 2) return n;
+    return climbStairs_recursion(n - 1) + climbStairs_recursion(n - 2);
+}
+
+int climbStairs_memo(int n) {
+    if (n <= 2) {
+        m[n] = n;
+        return m[n];
+    }
+    if (m.find(n) != m.end()) return m[n];
+    m[n] = climbStairs_memo(n - 1) + climbStairs_memo(n - 2);
+    return m[n];
+
+}
+
+int climbStairs_dp(int n) {
+    if (n <= 2) return n;
+    vector<int> dp ={0, 1, 2};
+    for (int i = 3; i <= n; ++i) {
+        dp.push_back(dp[i - 1] + dp[i - 2]);
+    }
+    return dp.back();
+}
+
+int main() {
+    //cout << "recursion : " << climbStairs_recursion(44) << endl;
+    //cout << "memo : " << climbStairs_memo(10000) << endl; // n = 10000, 0.018s
+    cout << "dp : " << climbStairs_dp(10000) << endl; // n = 10000, 0.008s
+
+    return 0;
+}
